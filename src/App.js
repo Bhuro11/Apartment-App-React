897b040cc79961f14caf8c4a13e911cb287298d0
Admin <Admin@DESKTOP-CCNHMQU>
2024-02-20 09:27:29 +0530

    day-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import TopHeader from './components/TopHeader';
+
 import HomePage from './components/user/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <TopHeader />
       <Routes>
         <Route path='/' element={<HomePage />} />
       </Routes>
--- src/components/user/HomePage.jsx
+++ src/components/user/HomePage.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
+import TopHeader from '../TopHeader'
 import Logo from '../images/HomeLogo.png'
 import { Link } from 'react-router-dom'
 
 export default function HomePage() {
   return (
-    <div className='m-0'>
-        <div className='home flex justify-evenly items-center' style = {{
-               backgroundImage:
-               'url("")',
-               backgroundSize: "contain",
-               backgroundRepeat: "no-repeat",
-            }}>
+    <div className='h-screen w-full ' >
+        <div className='home min-h-screen w-full' style = {{
+        backgroundImage:
+        `url("https://www.thespruce.com/thmb/zVrZlbIOVmH172Gkju3uCMpX2sc=/2048x0/filters:no_upscale():max_bytes(150000):strip_icc()/lavender2-dd461d67ee8240d5bbca3e0b09e9c41c.png")`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+     }}>
+        <TopHeader />
+        <div className='flex justify-around items-center mt-4' >
             <div className='flex justify-center items-center'>
                 <img src={Logo} alt="" />
             </div>
             <div>
-                <ul className='flex justify-center items-center gap-5'>
+                <ul className='flex justify-center items-center gap-9 text-2xl text-white'>
                     <li><Link to="/">HOME</Link></li>
                     <li><Link to="/">ABOUT US</Link></li>
                     <li><Link to="/">OUR SERVICE</Link></li>
@@ -23,8 +26,9 @@ export default function HomePage() {
                 </ul>
             </div>
             <div>
-                <button className='bg-white text-black p-2 text-xl'>Get Started</button>
+                <button className='bg-white p-2 text-[#588157] border border-[#588157] rounded-3xl w-36'>Get Started</button>
             </div>
+        </div>
         </div>
     </div>
   )
